8bfdf3de4acbc4329703c886880ef34fc296e0ec
sagor2315 <eeezahidul@gmail.com>
2024-05-22 10:08:33 +0600

    some new files created and some are modified for implementing products create, get, edit and delete

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,16 @@
 import express, { Request, Response } from "express";
 const app = express();
+import cors from "cors";
+import { productRoute } from "./ProductModules/EcommerceProduct/product.route";
+import { orderRoute } from "./OrderModules/ProductsOrder/order.route";
+
+// parser
+app.use(express.json());
+app.use(cors());
+
+// all apps routes
+app.use("/api/products", productRoute);
+app.use("/api/orders", orderRoute);
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello customers!");
